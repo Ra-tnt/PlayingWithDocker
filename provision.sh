1881f8982b0f2997b56fc97c979189089a42db1b
Ra-tnt <roody.tnt@gmail.com>
2020-10-09 20:54:09 +0300

    automated cloning

---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone https://github.com/microservices-demo/payment.git
+git clone https://github.com/microservices-demo/user.git
+git clone https://github.com/microservices-demo/front-end.git
+git clone https://github.com/microservices-demo/orders.git
+git clone https://github.com/microservices-demo/catalogue.git
+git clone https://github.com/microservices-demo/carts.git
+git clone https://github.com/microservices-demo/queue-master.git
+git clone https://github.com/microservices-demo/shipping.git
+git clone https://github.com/microservices-demo/msd-java.git
+git clone https://github.com/microservices-demo/orders.NET.git
+git clone https://github.com/microservices-demo/load-test.git
+git clone https://github.com/microservices-demo/e2e-tests.git
+
